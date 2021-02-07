bcc2d3d2aafc48e7b0229e299928e1451a732c6c
jllabarta <jllabarta@gmail.com>
2021-02-07 08:33:50 +0100

    Cambios
    Cambios

--- js/map.js
+++ js/map.js
@@ -3,6 +3,13 @@ var mapMain;
 // @formatter:off
 require([
         "esri/map",
+        "esri/geometry/Extent",
+        "esri/layers/ArcGISDynamicMapServiceLayer",
+        "esri/layers/FeatureLayer",
+        "esri/layers/FeatureType",
+        "esri/dijit/BasemapToggle",
+        "esri/dijit/Scalebar",
+        "esri/dijit/Legend",
 
         "dojo/ready",
         "dojo/parser",
@@ -10,7 +17,7 @@ require([
 
         "dijit/layout/BorderContainer",
         "dijit/layout/ContentPane"],
-    function (Map,
+    function (Map, Extent, ArcGISDynamicMapServiceLayer, FeatureLayer, FeatureType,BasemapToggle, Scalebar, Legend,
               ready, parser, on,
               BorderContainer, ContentPane) {
 // @formatter:on
@@ -26,37 +33,67 @@ require([
              * Note: Exact coordinates may vary slightly from snippet/solution
              * Reference: https://developers.arcgis.com/javascript/jssamples/fl_any_projection.html
              */
-
+            var extentInitial = new Extent({
+                "xmin":-14837446.937715188,
+                "ymin":4783149.319466121,
+                "xmax":-11877805.202513952,
+                "ymax":5279684.255206493,
+                "spatialReference":{"wkid":102100}
+            })
 
             // Create the map
             mapMain = new Map("cpCenter", {
-                basemap: "topo",
-                center: [-122.45, 37.75],
-                zoom: 12
+                basemap: "satellite",
+                extent: extentInitial
             });
 
             /*
              * Step: Add the USA map service to the map
              */
-
+            lyrUSA= new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer",{
+                opacity:0.5
+            });
 
             /*
              * Step: Add the earthquakes layer to the map
              */
-
+            lyrQuakes= new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Earthquakes_Since1970/FeatureServer/0");
+            lyrQuakes.setDefinitionExpression("magnitude>=2.0");
             /*
             * Step: Revise code to use the addLayers() method
             */
-
+            mapMain.addLayer(lyrUSA);
+            mapMain.addLayer(lyrQuakes);
             /*
              * Step: Add the BaseMapToggle widget to the map
              */
+            var toggle=new BasemapToggle({
+                map: mapMain,
+                basemap: "topo"},"BasemapToggle");
+                toggle.startup();
 
+            /*
+             * Step: Add a scalebar widget to the map
+             */
+            //mapMain.on(); // stub
+            var dijiScalebar=new Scalebar({
+                map : mapMain,
+                scalebarUnit: "dual",
+                attachTo: "bottom-left"
+            });
 
             /*
              * Step: Add a legend once all layers have been added to the map
              */
-            //mapMain.on(); // stub
+            mapMain.on("layer-add-result", function(){
+                var dijitLegend=new Legend ({
+                    map: mapMain,
+                    arrangement: Legend.ALIGN_RIGHT
+                },"divLegend");
+                dijitLegend.startup();
+            }); // stub
+
+
 
 
         });
